c8c0e7e663a89adc62c959383443796c2942a3f8
sabapapava24 <163457307+sabapapava24@users.noreply.github.com>
2024-03-21 10:54:53 +0400

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,19 @@
+ variable - ცვლადი
+variable name - ცვლადის სახელი
+variable value - ცვლადის მნიშვნელობა
+
+integer - მთელი რიცხვი
+writing - წერა
+running - გაშვება
+debugging - გასწორება
+parentheses - ფრჩხილები ()
+quotation marks - ბრჭყალები ""
+
+equal sign - ტოლობის ნიშანი =
+characters - სიმბოლო
+line by line - ხაზ-ხაზ
+
+
+/ - გაყოფა
+* - გამრავლება
+ )
